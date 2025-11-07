dddd710bcdf3da582f7c8eeba2dac3707a483b5d
BPB Online <41231825+bpbpublications@users.noreply.github.com>
2025-11-07 11:08:04 +0530

    By BPB Publications

--- Chapter 03/src/com/meennu/javabook/chap3/Manager.java
+++
@@ -1,9 +0,0 @@
-package com.meennu.javabook.chap3;
-
-public class Manager {
-
-	public Manager(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- Chapter 03/src/com/meennu/javabook/chap3/ProgramManager.java
+++
@@ -1,7 +0,0 @@
-package com.meennu.javabook.chap3;
-
-public class ProgramManager extends Employee {
-	ProgramManager(String name, int id) {
-		super(name, id);
-	}
-}
